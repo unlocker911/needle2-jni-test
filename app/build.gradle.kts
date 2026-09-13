4b82d837f11fc2c0038c1c4d7238c900f23a8e5d
Test User <test@example.com>
2026-09-13 22:13:48 +0300

    build: Pin Kotlin version to 2.0.21 to match Compose compiler

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,7 @@ import org.gradle.api.JavaVersion
 
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.android") version "2.0.21"
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.21"
 }
 
